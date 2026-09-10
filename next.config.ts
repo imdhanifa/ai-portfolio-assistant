efacb8a491249f915c7bdc3813eea464d19daa39
Hanifa <imdhanifa@gmail.com>
2026-09-10 13:32:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/ai-portfolio-assistant",
-  images: {
-    unoptimized: true,
-  },
+  
 };
 
 export default nextConfig;
